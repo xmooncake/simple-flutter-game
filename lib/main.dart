635b7fe30b06ee8d4638b861abcf23c4bf32ca4c
xmooncake <s.gierlachpl@gmail.com>
2024-05-26 21:54:19 +0200

    Add background music

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
+import 'dart:async';
+
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_flame_game/utilities/utilities.dart';
 
 void main() {
-  // runApp(const MyApp());
+  runApp(
+    GameWidget(
+      game: MyGame(),
+    ),
+  );
+}
+
+class MyGame extends FlameGame with KeyboardEvents {
+  @override
+  Color backgroundColor() => Colors.white;
+
+  @override
+  FutureOr<void> onLoad() async {
+    await FlameAudio.loop('background.mp3');
+
+    return super.onLoad();
+  }
 }
