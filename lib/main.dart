5e77580ae85ec5000fdbbafcd9733f769bdf3ecb
xmooncake <s.gierlachpl@gmail.com>
2024-06-05 23:21:05 +0200

    feat: Enable game state management with Flutter Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_flame_game/bloc/game_bloc.dart';
 import 'package:simple_flame_game/game.screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await FlameAudio.loop('background.mp3', volume: 0.5);
+  // await FlameAudio.loop('background.mp3', volume: 0.5);
 
   runApp(App());
 }
@@ -17,14 +19,9 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const Scaffold(
-        body: GameScreen(
-          usernameLeft: '',
-          scoreLeft: '',
-          usernameRight: '',
-          scoreRight: '',
-          roundNumber: '',
-        ),
+      home: BlocProvider(
+        create: (context) => GameBloc(),
+        child: const GameScreen(),
       ),
     );
   }
