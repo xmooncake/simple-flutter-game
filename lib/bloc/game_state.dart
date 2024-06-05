dc04f7a6bfcdcfe8a31d15c2859b86410698c0f8
xmooncake <s.gierlachpl@gmail.com>
2024-06-05 21:57:32 +0200

    add lottie assets, remove residue

--- lib/bloc/game_state.dart
+++ lib/bloc/game_state.dart
@@ -1,5 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'game_bloc.dart';
 
-sealed class GameState {}
+enum GameStatus { initial, win, lose }
 
-final class GameInitial extends GameState {}
+class GameState extends Equatable {
+  final GameStatus status;
+  final int userScore;
+  final int computerScore;
+
+  const GameState({
+    this.status = GameStatus.initial,
+    this.userScore = 0,
+    this.computerScore = 0,
+  });
+
+  GameState copyWith({
+    GameStatus? status,
+    int? userScore,
+    int? computerScore,
+  }) {
+    return GameState(
+      status: status ?? this.status,
+      userScore: userScore ?? this.userScore,
+      computerScore: computerScore ?? this.computerScore,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, userScore, computerScore];
+}
